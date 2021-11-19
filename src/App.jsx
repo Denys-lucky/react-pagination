a9040fbb9dfe828953c38fa01552e9ca144addc8
Denys <denysexample5@gmail.com>
2021-11-19 22:03:59 +0200

    add git

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Container } from '@mui/material'
+import {Switch, Route} from 'react-router-dom'
+
+import { HomePage } from './HomePage'
+import { About } from './About'
+import { NotFound } from './NotFound'
+
+
+function App() {
+  return (
+    <Container sx={{marginTop: 5}} maxWidth="md" >
+      <Switch>
+        <Route exact path="/"  component={HomePage} />
+        <Route exact path="/about"  component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/NotFound.jsx
@@ -0,0 +1,9 @@
+export const NotFound = () => {
+    return (
+        <div>
+            Page doesn't exist
+        </div>
+    )
+}
+
+export default NotFound;
